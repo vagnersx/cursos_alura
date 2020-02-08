5c80dcbee1e41e2d376f3ae46144f61f85105d7e
vagner <vagner192@gmail.com>
2020-02-08 13:04:31 -0300

    Primeiros testes

---
+++ java-p1-pp/OlaMundo.java
@@ -0,0 +1,8 @@
+import pacote.Teste;
+
+class OlaMundo {
+    public static void main(String args[]) {
+       Teste teste = new Teste();
+       teste.printar();
+    }
+}
---
+++ java-p1-pp/pacote/Teste.java
@@ -0,0 +1,9 @@
+package pacote;
+
+public class Teste {
+    public void printar() {
+        System
+            .out
+            .println("Printei");
+    }
+}
