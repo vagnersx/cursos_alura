4d2d63d85e7da4a1ec140cd06dc4a9cbf7689692
vagner <vagner192@gmail.com>
2020-02-25 21:13:00 -0300

    Spring boot parte 2 - Aula 3

--- springboot-p2/forum/src/main/java/br/com/vagner/controller/TopicosController.java
+++ springboot-p2/forum/src/main/java/br/com/vagner/controller/TopicosController.java
@@ -7,6 +7,7 @@ import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
@@ -42,6 +43,7 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
+	//@Cacheable(value = "listaDeTopicos")
 	public Page<TopicoDTO> listar(@RequestParam(required = false) String nomeCurso,
 			@PageableDefault(direction = Direction.DESC, sort = "id", page = 0, size = 10) Pageable paginacao) {
 		// @RequestParam int pagina, @RequestParam int qtd, @RequestParam String
@@ -63,6 +65,7 @@ public class TopicosController {
 	
 	@PostMapping
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true ) // Limpar o cache
 	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
 		Topico topico = topicoForm.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -85,6 +88,7 @@ public class TopicosController {
 	
 	@PutMapping(path = "/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true )
 	public TopicoDTO atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm topicoForm) throws Exception{
 		
 		Topico topico = topicoForm.atualizar(id, topicoRepository);
@@ -93,6 +97,7 @@ public class TopicosController {
 	
 	@DeleteMapping(path = "/{id}")
 	@Transactional
+	@CacheEvict(value = "listaDeTopicos", allEntries = true )
 	public void remover(@PathVariable Long id) {
 		topicoRepository.deleteById(id);
 	}
